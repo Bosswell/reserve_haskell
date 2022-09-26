6e65a270aaa617798d1aaa6888fec99878f734e3
Jakub Batko <bosswell9@gmail.com>
2022-09-26 16:56:41 +0200

    Save

--- app/Main.hs
+++ app/Main.hs
@@ -3,38 +3,51 @@ module Main where
 import Control.Monad
 import Data.Time
 
---data GenerateEventsStruct = GenerateEvents {
---    subjectId :: Int,
---    eventDuration :: Int,
---    includeWeekend :: Bool,
---    eventGenerationFlow :: Int,
---    dateFrom :: Maybe DateTime,
---    dateTo :: Maybe DateTime,
---    hourFrom :: Maybe TimeOfDay,
---    hourTo :: Maybe TimeOfDay,
---    selectedDays :: [Maybe DateTime]
---
---} deriving Show;
+data EventGenerationFlow = Select [Day] | Period Day Day deriving Show
+
+data GenerateEventsStruct = GenerateEvents {
+    subjectId :: Int,
+    eventDuration :: Int,
+    includeWeekend :: Bool,
+    eventGenerationFlow :: EventGenerationFlow,
+    hourFrom :: TimeOfDay,
+    hourTo :: TimeOfDay,
+} deriving Show;
 
 makeUTCTimeValid :: Day -> TimeOfDay -> UTCTime
 makeUTCTimeValid d tod = UTCTime { utctDay = d, utctDayTime = timeOfDayToTime tod}
 
-filterWeekDays :: Bool -> [Day] -> [Day]
-filterWeekDays True days = days
-filterWeekDays _ days = filter (\day -> let dof = dayOfWeek day in dof `elem` [Saturday, Sunday]) days
+filterWeekend :: Bool -> [Day] -> [Day]
+filterWeekend True days = days
+filterWeekend _ days = filter (\day -> let dof = dayOfWeek day in dof `elem` [Saturday, Sunday]) days
+
+normalizeMinutes :: Int -> Int
+normalizeMinutes minutes
+                | minutes `mod` 5 == 0 = minutes
+                | otherwise = ((minutes `div` 5) + 1) * 5
 
 f = do
     dayFrom <- fromGregorianValid 2010 3 4
     dayTo <- fromGregorianValid 2010 3 10
 
-    let includeWeekend = False;
+    let dto = GenerateEvents {
+        subjectId = 1,
+        eventDuration = 30 * 60,
+        includeWeekend = False,
+        eventGenerationFlow = Period dayFrom dayTo
+--        hourFrom = makeTimeOfDayValid 10 30 00,
+--        hourTo = makeTimeOfDayValid 11 50 00
+    }
 
-    return $ map getDatePeriod (filterWeekDays includeWeekend [dayFrom .. dayTo])
+    return $ map getDatePeriod (filterWeekend True [dayFrom .. dayTo])
+
+--createEvent :: Int -> (UTCTime, UTCTime)
+--createEvent subjectId
 
 getDatePeriod :: Day -> Maybe [(UTCTime, UTCTime)]
 getDatePeriod day = do
     hourFrom <- makeTimeOfDayValid 10 30 00
-    hourTo <- makeTimeOfDayValid 12 00 00
+    hourTo <- makeTimeOfDayValid 11 50 00
 
     let eventDuration = 1800 :: NominalDiffTime
     let eventGap = 0 :: NominalDiffTime
@@ -44,8 +57,10 @@ getDatePeriod day = do
 createEventsPeriods :: UTCTime -> UTCTime -> NominalDiffTime -> NominalDiffTime -> [(UTCTime, UTCTime)]
 createEventsPeriods utcFrom utcTo eventDuration eventGap
     | utcFrom >= utcTo = []
+    | utcTo < utcToInPeriod = []
     | otherwise =
-        [(utcFrom, addUTCTime (eventDuration - 1) utcFrom)] ++
+        [(utcFrom, utcToInPeriod)] ++
         createEventsPeriods (addUTCTime (eventDuration + eventGap) utcFrom) utcTo eventDuration eventGap
+    where utcToInPeriod = addUTCTime (eventDuration - 1) utcFrom
 
 main = print $ f
